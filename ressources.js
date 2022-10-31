b5a63f94af24688dbcca53350b50bdc20b54f973
Thomas Ayissi <thomas8sea@gmail.com>
2022-10-31 10:44:51 +0100

    Create ressources.js

---
+++ ressources.js
@@ -0,0 +1,26 @@
+// 1 - Exercice URL
+// https://edabit.com/challenge/4gzDuDkompAqujpRi
+
+/*
+Create a function that takes a number as an argument. 
+Add up all the numbers from 1 to the number you passed to the function. F
+or example, if the input is 4 then your function should return 10 because 1 + 2 + 3 + 4 = 10.
+*/
+
+
+/*
+Examples
+
+addUp(4) ➞ 10
+
+addUp(13) ➞ 91
+
+addUp(600) ➞ 180300
+Notes
+
+Expect any positive number between 1 and 1000.
+*/
+
+
+// 2 - Course Repo URL
+// https://github.com/HackYourFutureBelgium/behavior-strategy-implementation/blob/master/README.md
